f9eb4e629a8081c9732e62709e564d31f371b6fc
poby123 <youngsm12@naver.com>
2021-04-01 23:23:23 +0900

    delete client-backup

--- client-backup/src/components/SpacingComponent.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { View } from 'react-native';
-
-export default function SpacingComponent({ spacing }) {
-  return <View style={{ minHeight: `${spacing * 0.5}em` }}></View>;
-}
